ccfa1f80e275691de0e647a63e3221f476938aa7
Chii Yuen <chii.yuen@hotmail.com>
2024-07-28 07:51:12 +0800

    Format code

--- models/crowdfund.model.ts
+++ models/crowdfund.model.ts
@@ -1,9 +1,8 @@
 export interface ICrowdFund {
-    _id: string;
-    requester: string;
-    description: string;
-    amountRequested: number;
-    currency: string;
-    dateRequested: Date;
-  }
-  
+  _id: string;
+  requester: string;
+  description: string;
+  amountRequested: number;
+  currency: string;
+  dateRequested: Date;
+}
