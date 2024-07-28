0dec079be799bed4cd152109105c1dd2b2966430
Vincent <vincent.wesley.liem@gmail.com>
2024-07-28 09:08:22 +0800

    modal padding

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Modal from 'react-modal';
+import { Styles } from 'react-modal';
 
 type ModalProps = {
   isOpen: boolean;
@@ -17,11 +18,13 @@ const Model = ({ isOpen, setIsOpen, isValidatorRunning, setIsValidatorRunning }:
       top: '50%',
       left: '50%',
       right: 'auto',
+      width: "80vw",
       bottom: 'auto',
       marginRight: '-50%',
       transform: 'translate(-50%, -50%)',
       backgroundColor: "#181818",
       border: "none",
+      padding: "0px",
     },
   };
 
@@ -31,27 +34,29 @@ const Model = ({ isOpen, setIsOpen, isValidatorRunning, setIsValidatorRunning }:
         isOpen={isOpen}
         ariaHideApp={false}
         onRequestClose={() => setIsOpen(false)}
-        style={customStyles}
+        style={customStyles as Styles}
       >
-        <h1 className="font-semibold p-2">Would you like to {isValidatorRunning ? "stop" : "run"} your validator?</h1>
-        <div className = "flex justify-between items-center mt-2">
-          <button
-            type = "button"
-            className = "bg-accent text-background rounded-lg font-semibold p-3"
-            onClick = {() => {
-              setIsValidatorRunning(!isValidatorRunning);
-              console.log(isValidatorRunning)
-              setIsOpen(false);
-            }}
-          >
-            {isValidatorRunning ? "Stop Validator" : "Run Validator"}
-          </button>
-          <button
-          type="button"
-          className="bg-red-500 text-background rounded-lg font-semibold p-3"
-          onClick = {() => setIsOpen(false)}>
-            Cancel
-          </button>
+        <div className="p-5 h-full">
+          <h1 className="font-semibold p-2">Would you like to {isValidatorRunning ? "stop" : "run"} your validator?</h1>
+          <div className = "flex justify-between items-center my-2">
+            <button
+              type = "button"
+              className = "bg-accent text-background rounded-lg font-semibold p-3"
+              onClick = {() => {
+                setIsValidatorRunning(!isValidatorRunning);
+                console.log(isValidatorRunning)
+                setIsOpen(false);
+              }}
+            >
+              {isValidatorRunning ? "Stop Validator" : "Run Validator"}
+            </button>
+            <button
+            type="button"
+            className="bg-red-500 text-background rounded-lg font-semibold p-3"
+            onClick = {() => setIsOpen(false)}>
+              Cancel
+            </button>
+          </div>
         </div>
       </Modal>
     </div>
